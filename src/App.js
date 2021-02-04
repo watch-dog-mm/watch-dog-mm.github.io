7751558ed56ad734a2dcc3a7cd21b94dcabd4266
user <win@ezysign.cc>
2021-02-04 22:32:20 +0630

    added markers and firebase integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import { useEffect, useState } from "react";
 import { ChangeView } from "./ChangeView";
+import MapMarker, { PersonMarker } from "./Components/marker";
+import firebase from "firebase";
+import { config } from "./config";
+import {
+  FirebaseDatabaseNode,
+  FirebaseDatabaseProvider,
+} from "@react-firebase/database";
 
 function App() {
   const [latlng, setlatlng] = useState({ lat: 21.9162, lng: 95.956 });
@@ -18,35 +25,56 @@ function App() {
 
   return (
     <>
-      <select
-        onChange={(e) => {
-          setlatlng({
-            lat: parseFloat(e.target.value.split("|")[0]),
-            lng: parseFloat(e.target.value.split("|")[1]),
-          });
-        }}
-      >
-        <option value="16.8409|96.1735">Yangon</option>
-        <option value="21.9588|96.0891" selected>
-          Mandalay
-        </option>
-        <option value="19.7633|96.0785">Naypyi Daw</option>
-      </select>
-      <MapContainer
-        style={{ height: `100vh` }}
-        center={[latlng.lat, latlng.lng]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <ChangeView center={[latlng.lat, latlng.lng]} zoom={13} />
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[latlng.lat, latlng.lng]}>
-          <Popup>This is your location</Popup>
-        </Marker>
-      </MapContainer>
+      <FirebaseDatabaseProvider firebase={firebase} {...config}>
+        <select
+          onChange={(e) => {
+            setlatlng({
+              lat: parseFloat(e.target.value.split("|")[0]),
+              lng: parseFloat(e.target.value.split("|")[1]),
+            });
+          }}
+        >
+          <option value="16.8409|96.1735" selected>
+            Yangon
+          </option>
+          <option value="21.9588|96.0891">Mandalay</option>
+          <option value="19.7633|96.0785">Naypyi Daw</option>
+        </select>
+        <MapContainer
+          style={{ height: `100vh` }}
+          center={[latlng.lat, latlng.lng]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <ChangeView center={[latlng.lat, latlng.lng]} zoom={13} />
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <PersonMarker
+            position={[latlng.lat, latlng.lng]}
+            message="This is your current location"
+          />
+          <FirebaseDatabaseNode
+            path="locations"
+            // orderByKey
+          >
+            {(d) => {
+              console.log(d);
+              return (
+                <>
+                  {!d.isLoading &&
+                    Array.isArray(d.value) &&
+                    d.value.map((item) => {
+                      console.log(item);
+                      return <MapMarker {...item} />;
+                    })}
+                </>
+              );
+            }}
+          </FirebaseDatabaseNode>
+        </MapContainer>
+      </FirebaseDatabaseProvider>
     </>
   );
 }
